035d150ed1788f5c01da97b320a1e83975e9949e
ratnesh90859 <122274091+ratnesh90859@users.noreply.github.com>
2025-04-13 19:25:01 +0530

    llm_helper.py

--- QuickPost/llm_helper.py
+++ QuickPost/llm_helper.py
@@ -2,15 +2,21 @@ from langchain_groq import ChatGroq
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama3-70b-8192")
+try:
+    load_dotenv()
+except:
+    
+    pass
 
+
+groq_api_key = os.getenv("GROQ_API_KEY")
+if not groq_api_key:
+    
+    import streamlit as st
+    groq_api_key = st.secrets.get("GROQ_API_KEY", "")
+
+llm = ChatGroq(groq_api_key=groq_api_key, model_name="llama3-70b-8192")
 
 if __name__ == "__main__":
-    response = llm.invoke("Two most important ingradient in samosa are ")
+    response = llm.invoke("Two most important ingredients in samosa are ")
     print(response.content)
-
-
-
-
-
